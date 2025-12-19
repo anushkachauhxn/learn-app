a6f9e13fd145ae1c018cfd51b1914572325d43bb
Anushka Chauhan <anushka.chauhan@mygate.in>
2025-12-19 15:24:24 +0530

    Generate all modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CoursesModule } from './courses/courses.module';
+import { LessonsModule } from './lessons/lessons.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [CoursesModule, LessonsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
